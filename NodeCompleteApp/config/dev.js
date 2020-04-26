8f03446aff7e76419ce1fa71115ebf38142ce3a8
asharma <Aatish.Sharma@connectwise.com>
2020-04-26 20:21:59 +0530

    adding OAUTH

--- NodeCompleteApp/config/dev.js
+++ NodeCompleteApp/config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    '168596808113-92cbl4n51e36q0pj215tv790042tskcd.apps.googleusercontent.com',
+  googleClientSecret: 'Cekk0EnmQVN3TZ-tIr_lz34Z',
+  redirectURI: 'http://localhost:3000/blogs',
+  mongoURI: 'mongodb://localhost:27017/blog_everyone',
   cookieKey: '123123123'
 };
